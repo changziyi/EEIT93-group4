be72d8a922d2dc0571a2c4f56bc0fe28459d578b
王晊衡 <changziyi@gmail.com>
2017-05-02 18:11:58 +0800

    11111

---
+++ ToolMan/src/toolman/order/controller/OrderController.java
@@ -0,0 +1,164 @@
+package toolman.order.controller;
+
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import toolman.order.model.DeptService;
+import toolman.order.model.DeptVO;
+import toolman.order..model.*;
+
+public class OrderController extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		
+		
+		
+		if ("getOne_For_Update".equals(action)) { // 來自listAllEmp.jsp 或  /dept/listEmps_ByDeptno.jsp 的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			String requestURL = req.getParameter("requestURL"); // 送出修改的來源網頁路徑: 可能為【/emp/listAllEmp.jsp】 或  【/dept/listEmps_ByDeptno.jsp】 或 【 /dept/listAllDept.jsp】		
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				Integer empno = new Integer(req.getParameter("empno"));
+				
+				/***************************2.開始查詢資料****************************************/
+				EmpService empSvc = new EmpService();
+				EmpVO empVO = empSvc.getOneEmp(empno);
+								
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				req.setAttribute("empVO", empVO); // 資料庫取出的empVO物件,存入req
+				String url = "/emp/update_emp_input.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交update_emp_input.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理************************************/
+			} catch (Exception e) {
+				errorMsgs.add("修改資料取出時失敗:"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher(requestURL);
+				failureView.forward(req, res);
+			}
+		}
+		
+		
+		if ("update".equals(action)) { // 來自update_emp_input.jsp的請求
+			
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			String requestURL = req.getParameter("requestURL"); // 送出修改的來源網頁路徑: 可能為【/emp/listAllEmp.jsp】 或  【/dept/listEmps_ByDeptno.jsp】 或 【 /dept/listAllDept.jsp】
+		
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				Integer empno = new Integer(req.getParameter("empno").trim());
+				String ename = req.getParameter("ename").trim();
+				String job = req.getParameter("job").trim();				
+				
+		
+
+				Integer deptno = new Integer(req.getParameter("deptno").trim());
+
+				OrderVO orderVO = new OrderVO();
+				orderVO.setO_id(o_id);
+				
+				orderVO.setM_rating(m_rating);
+				orderVO.setC_rating(c_rating);
+				orderVO.setCa_des(ca_des);
+				orderVO.setMa_des(ma_des);
+			
+				//empVO.setDeptno(deptno);
+				//以下3行程式碼因為要配合Hibernate的empVO,以能夠使用Hibernate的強大功能,所以這裏顯得比較麻煩!!
+		
+
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					req.setAttribute("orderVO", orderVO); // 含有輸入格式錯誤的empVO物件,也存入req
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/emp/update_emp_input.jsp");
+					failureView.forward(req, res);
+					return; //程式中斷
+				}
+				
+				/***************************2.開始修改資料*****************************************/
+				OrderService orderSvc = new OrderService();
+				orderVO = orderSvc.updateOrder(o_id, m_rating,c_rating, ca_des, ma_des );
+			
+				/***************************3.修改完成,準備轉交(Send the Success view)*************/				
+		//		DeptService deptSvc = new DeptService();
+		//		if(requestURL.equals("/dept/listEmps_ByDeptno.jsp") || requestURL.equals("/dept/listAllDept.jsp"))
+		//			req.setAttribute("listEmps_ByDeptno",deptSvc.getEmpsByDeptno(deptno)); // 資料庫取出的list物件,存入request
+
+                String url = requestURL;
+				RequestDispatcher successView = req.getRequestDispatcher(url);   // 修改成功後,轉交回送出修改的來源網頁
+				successView.forward(req, res);
+			
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("修改資料失敗:"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/emp/update_emp_input.jsp");
+				failureView.forward(req, res);
+			}
+		
+		
+			
+		
+       
+//		if ("delete".equals(action)) { // 來自listAllEmp.jsp 或  /dept/listEmps_ByDeptno.jsp的請求
+//
+//			List<String> errorMsgs = new LinkedList<String>();
+//			// Store this set in the request scope, in case we need to
+//			// send the ErrorPage view.
+//			req.setAttribute("errorMsgs", errorMsgs);
+//			
+//			String requestURL = req.getParameter("requestURL"); // 送出刪除的來源網頁路徑: 可能為【/emp/listAllEmp.jsp】 或  【/dept/listEmps_ByDeptno.jsp】 或 【 /dept/listAllDept.jsp】
+//
+//			try {
+//				/***************************1.接收請求參數***************************************/
+//				Integer o_id = new Integer(req.getParameter("empno"));
+//				
+//				/***************************2.開始刪除資料***************************************/
+//				OrderService orderSvc = new OrderService();
+//				OrderVO orderVO = orderSvc.getOneEmp(o_id);
+//				empSvc.deleteEmp(o_id);
+//				
+//				/***************************3.刪除完成,準備轉交(Send the Success view)***********/
+//				DeptService deptSvc = new DeptService();
+//				if(requestURL.equals("/dept/listEmps_ByDeptno.jsp") || requestURL.equals("/dept/listAllDept.jsp"))
+//				  //req.setAttribute("listEmps_ByDeptno",deptSvc.getEmpsByDeptno(empVO.getDeptno())); // 資料庫取出的list物件,存入request
+//				    req.setAttribute("listEmps_ByDeptno",deptSvc.getEmpsByDeptno(empVO.getDeptVO().getDeptno())); // 資料庫取出的list物件,存入request
+//				
+//				String url = requestURL;
+//				RequestDispatcher successView = req.getRequestDispatcher(url); // 刪除成功後,轉交回送出刪除的來源網頁
+//				successView.forward(req, res);
+//				
+//				/***************************其他可能的錯誤處理**********************************/
+//			} catch (Exception e) {
+//				errorMsgs.add("刪除資料失敗:"+e.getMessage());
+//				RequestDispatcher failureView = req
+//						.getRequestDispatcher(requestURL);
+//				failureView.forward(req, res);
+//			}
+//		}
+	}
+}
+}
