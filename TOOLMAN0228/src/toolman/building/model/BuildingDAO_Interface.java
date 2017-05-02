27c1cdff042bb8936f60ae92e851b2eab1812a6f
Benny <chang@changziyi-pc>
2017-05-03 01:01:12 +0800

    15

--- TOOLMAN0228/src/toolman/building/model/BuildingDAO_Interface.java
+++
@@ -1,6 +0,0 @@
-package toolman.building.model;
-
-public interface BuildingDAO_Interface {
-
-	public BuildingVO getBuildingByType(Integer h_type);
-}
--- TOOLMAN0228/src/toolman/order/controller/recommandation.java
+++
@@ -1,5 +0,0 @@
-package toolman.order.controller;
-
-public class recommandation {
-
-}
