55cd6755825a58c2d62405bda2c7bdc3ad0626d8
Benny <Student@192.168.34.32>
2017-05-08 11:47:07 +0800

    benny20th commit cdata

--- ToolMan/src/toolman/cdata/model/CdataDAO.java
+++ ToolMan/src/toolman/cdata/model/CdataDAO.java
@@ -114,7 +114,21 @@ public class CdataDAO implements CdataDAO_interface{
 		}
 		return cdataVO;
 	}
-	
+	//BY Benny
+	public void updatecustomerSname(Integer c_id) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			Query query = session.createQuery("from CdataVO set s_name=:s where c_id=:c");
+			query.setParameter("s","c_sus");
+			query.setParameter("c",c_id);
+			Integer count = query.executeUpdate();
+			session.getTransaction().commit();
+		} catch (RuntimeException ex) {
+			session.getTransaction().rollback();
+			throw ex;
+		}
+	}
 
 	public static void main(String args[]){		
 		CdataDAO  dao = new CdataDAO();
